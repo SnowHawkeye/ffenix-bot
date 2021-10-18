c50f5b62d352544d1756f1fe358baa23808bcd5f
SnowHawkeye <hadriensalem@gmail.com>
2021-10-18 22:12:33 +0200

    Add simple ping pong command

--- src/main/kotlin/bot/BotMain.kt
+++ src/main/kotlin/bot/BotMain.kt
@@ -3,5 +3,12 @@ package bot
 import com.jessecorbett.diskord.bot.*
 
 suspend fun main() {
-
+    val token = System.getenv(FFENIX_BOT_TOKEN_KEY)
+    bot(token) {
+        classicCommands(commandPrefix = "!") {
+            command("ping") {
+                it.respond("pong")
+            }
+        }
+    }
 }
---
+++ src/main/kotlin/bot/Constants.kt
@@ -0,0 +1,3 @@
+package bot
+
+const val FFENIX_BOT_TOKEN_KEY = "FFENIX_BOT_TOKEN"
