a7db095ee2ffc43dd2a6a63566e8d5aec9ba32f9
SnowHawkeye <hadriensalem@gmail.com>
2021-10-18 23:56:05 +0200

    Add jar task in  build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,26 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.jessecorbett:diskord-bot:2.1.1")
+
+    implementation("io.ktor:ktor-server-core:1.6.4")
+    implementation("io.ktor:ktor-server-netty:1.6.4")
+    implementation("ch.qos.logback:logback-classic:1.2.5")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+tasks.withType<Jar>() {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes["Main-Class"] = "bot/BotMainKt" }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
 tasks.withType<KotlinCompile>() {
