3e73f6ad51e56534173e6bba9297ba3aee36a990
SnowHawkeye <48864485+SnowHawkeye@users.noreply.github.com>
2021-10-26 14:00:52 +0200

    Add dependencies for API calling, logging and JSON handling - GSON dependency - Retrofit dependency - OkHttp dependency - JColor dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,33 @@ repositories {
 }
 
 dependencies {
+    // Versions
+    val kordVersion = "0.8.0-M7"
+    val log4jVersion = "2.14.1"
+    val gsonVersion = "2.8.8"
+    val retrofitVersion = "2.9.0"
+    val okHttpVersion = "4.9.0"
+    val jColorVersion = "5.0.1"
+
     testImplementation(kotlin("test"))
 
-    val kordVersion = "0.8.0-M7"
+    // Kord
     implementation("dev.kord:kord-core:$kordVersion")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion") // required to fix logging
+
+    // GSON
+    implementation("com.google.code.gson:gson:$gsonVersion")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+
+    // JColor
+    implementation("com.diogonunes:JColor:$jColorVersion")
 }
 
 tasks {
